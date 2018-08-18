5c1cb660761f64776f7bcd6ec064318bf0b0318d
afeiship <1290657123@qq.com>
2018-08-18 10:37:16 +0800

    issue: udaptegs

--- index.sh
+++ index.sh
@@ -7,5 +7,12 @@ alias gmi='gem install';
 alias gm-swith='gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/';
 alias gm-list='gem sources -l';
 
+## set gem path:
+if [ -d "$HOME/.gem" ]; then
+    export GEM_HOME=$HOME/.gem;
+    export GEM_PATH=$HOME/.gem;
+fi
+
 ##bundle commands:
-alias bdi='sudo bundle install';
+alias bdi='bundle install';
+alias sbdi='sudo bundle install';

6ac2ddfa549fb218fbc01cd8040e5790fae6cee3
afeiship <1290657123@qq.com>
2018-08-21 21:25:48 +0800

    issue: append path to ENV:PATH

--- index.sh
+++ index.sh
@@ -11,6 +11,7 @@ alias gm-list='gem sources -l';
 if [ -d "$HOME/.gem" ]; then
     export GEM_HOME=$HOME/.gem;
     export GEM_PATH=$HOME/.gem;
+    export PATH=$PATH:$GEM_PATH
 fi
 
 ##bundle commands:
